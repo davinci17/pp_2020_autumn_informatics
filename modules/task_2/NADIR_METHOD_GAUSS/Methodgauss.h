f8f8e5c2ebdf9a89d439db2d3ae0fd5c89a9ed8b
NADIR Mohammed <63372033+davinci17@users.noreply.github.com>
2020-11-24 00:34:55 -1200

    Update Methodgauss.h

--- modules/task_2/NADIR_METHOD_GAUSS/Methodgauss.h
+++ modules/task_2/NADIR_METHOD_GAUSS/Methodgauss.h
@@ -2,11 +2,10 @@
 #define MODULES_TASK_2_NADIR_M_HORIZONTAL_METHOD_GAUSS_HORIZONTAL_METHOD_H_
 
 
-#include<vector>
+
 
 void methodGauss(const double* array, double* solution,int row);
-
- void methodGaussParallel(const double* array, double* solution, int row, int col);
+void methodGaussParallel(const double* array, double* solution, int row, int col);
  
 #endif // MODULES_TASK_2_NADIR_M_HORIZONTAL_METHOD_GAUSS_HORIZONTAL_METHOD_H_
 
