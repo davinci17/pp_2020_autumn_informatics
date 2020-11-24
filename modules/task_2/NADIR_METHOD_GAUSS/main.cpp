3a746fe09fcc9d7a8af252acee71e8bdf70601e8
NADIR Mohammed <63372033+davinci17@users.noreply.github.com>
2020-11-23 23:41:25 -1200

    Update main.cpp

--- modules/task_2/NADIR_METHOD_GAUSS/main.cpp
+++ modules/task_2/NADIR_METHOD_GAUSS/main.cpp
@@ -3,54 +3,45 @@
 #include<mpi.h>
 #include "../../../modules/task_2/nadir_method_gauss/methodGauss.h"
 
-
 TEST(GAUSS_PARALLEL_MPI, 2x3)
 {
     int rank, size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int row = 2;
-    int col = 3;
+    int row = 2, col = 3;
     double* array{ new double[row * col]{ 1,2,3,4,5,6} };
-    double* solution = new double[row];
-    //part2
-    double* sub_vec2 = new double[row * col];
-    sub_vec2 = methodGaussParallel(array, solution);
+    double* sub_solution1 = new double[row];
 
-    std::vector<std::vector<double>>array1{ {1,2,3},{4,5,6} };
-    std::vector<std::vector<double>> sub_vec1 = methodGauss(array1);
-    if (rank == 0) {
-        for (int i = 0; i < row; i++) {ASSERT_EQ(sub_vec1[i], sub_vec2[i]);}
+    methodGaussParallel(array, sub_solution1);
+    if (rank == 0) {double* sub_solution2 = new double[row];
+        methodGauss(array, sub_solution2);
+        for (int i = 0; i < row; i++) {ASSERT_EQ(sub_solution1[i], sub_solution2[i]);}
+        delete[] sub_solution2;
     }
     delete[] array;
-    delete[] solution;
-    delete[] sub_vec2;
+    delete[] sub_solution1;
 }
-
-
 TEST(GAUSS_PARALLEL_MPI, 3x4)
 {
     int rank, size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int row = 3;
-    int col = 4;
+    int row = 3, col = 4;
     double* array{ new double[row * col]{ 1,2,3,4,5,6,7,8,9,10,11,12 } };
-    double* solution = new double[row];
-    double* sub_vec2 = new double[row * col];
+    double* sub_solution1 = new double[row];
 
-    sub_vec2 = methodGaussParallel(array, solution);
-
-    std::vector<std::vector<double>>array1{ {1,2,3,4} ,{5,6,7,8},{9,10,11,12} };
-    std::vector<std::vector<double>> sub_vec1 = methodGauss(array1);
-    if (rank == 0) {
-        for (int i = 0; i < row; i++) { ASSERT_EQ(sub_vec1[i], sub_vec2[i]); }
+    methodGaussParallel(array, sub_solution1);
+    if (rank == 0) {double* sub_solution2 = new double[row];
+        methodGauss(array, sub_solution2);
+        for (int i = 0; i < row; i++) { ASSERT_EQ(sub_solution1[i], sub_solution2[i]); }
+        delete[] sub_solution2;
     }
     delete[] array;
-    delete[] solution;
-    delete[] sub_vec2;
-
+    delete[] sub_solution1;
 }
+
+
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     MPI_Init(&argc, &argv);
