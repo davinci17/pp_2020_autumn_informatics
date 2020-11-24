02228ac1eed9eade2b9ce3deb3aeb09766c8598c
NADIR Mohammed <63372033+davinci17@users.noreply.github.com>
2020-11-23 12:32:06 -1200

    Update Methodgauss.cpp

--- modules/task_2/NADIR_METHOD_GAUSS/Methodgauss.cpp
+++ modules/task_2/NADIR_METHOD_GAUSS/Methodgauss.cpp
@@ -1,134 +1,136 @@
-#include "../../../modules/task_2/nadir_method_gauss/methodGauss.h"
 #include<iostream>
 #include<vector>
 #include<random>
 #include<iomanip>
 #include<mpi.h>
+#include "../../../modules/task_2/nadir_method_gauss/methodGauss.h"
 
 
-void displayVector(std::vector<std::vector<double>> vec) {
-    std::cout << std::setprecision(2) << std::fixed;
-    for (int i = 0; i < vec.size(); i++) {
-        for (int j = 0; j < vec.size(); j++) {
-            std::cout << vec[i][j] << "\t";
+int row, col;
+std::vector<std::vector<double>> methodGauss(std::vector<std::vector<double>>array) {
 
-        }
-        std::cout << std::endl;
-    }
-}
-std::vector<std::vector<double>> GenerateRandom()
-{
-    std::cout << std::setprecision(2) << std::fixed << std::endl;
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> distr1(2, 5);
-    int size = distr1(gen);
-    std::vector<std::vector<double>> vec(size);
-
-    for (int i = 0; i < size; i++)
+    double* x = new double[row];
+    double temp, s;
+    for (int j = 0; j < row - 1; j++)
     {
-        for (int j = 0; j < size; j++)
+        for (int i = j + 1; i < row; i++) {
+            temp = array[i][j] / array[j][j];
+            for (int k = 0; k < row + 1; k++) { array[i][k] -= array[j][k] * temp; }
+        }
+    }
+    std::cout << "\n --------------------------------------------------------------------------\n";
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < row + 1; j++)
         {
-            std::uniform_real_distribution<double> distr(0.0, 9.0);
-            vec[i].push_back(distr(gen));
-        }
-        std::cout << std::endl;
+            std::cout << std::setw(8) << std::setprecision(4) << std::fixed << array[i][j];
+        } std::cout << std::endl;
     }
-    // displayVector(vec);
-    std::cout << "********************************" << std::endl;
-    return vec;
-}
-std::vector<std::vector<double>> methodGauss(std::vector<std::vector<double>> vec)
-{
-
-    vec = GenerateRandom();
-    // vector<std::vector<double>> vec{ {1,2} ,{3,4} };
-    double result = 0;
-    for (int i = 0; i < vec.size(); i++)
+    std::cout << "\n --------------------------------------------------------------------------\n";
+    for (int i = row - 1; i >= 0; i--)
     {
-        if (vec[i][i] != 0.0) {
-            for (int j = 0; j < vec[i].size(); j++)
-            {
-                if (i != j)
-                {
-                    result = vec[j][i] / vec[i][i];
+        s = 0;
+        for (int j = i + 1; j < row; j++) {
+            s += array[i][j] * x[j];
+            x[i] = (array[i][row] - s) / array[i][i];
+        }
+    }
+    return array;
+}
 
-                    for (int k = 0; k < vec[i].size(); k++)
-                    {
-                        vec[j][k] = vec[j][k] - result * vec[i][k];
-                    }
-                }
+double* methodGaussParallel(const double* array, double* solution) {
+    int size, rank;
+    double* array_temp = new double[row * col];
+    for (int i = 0; i < row * col; i++) { array_temp[i] = array[i]; }
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int n_Row = row / size;
+    int rest_Row = row % size;
+
+    int stan_Row;
+    if (rank < rest_Row) { stan_Row = n_Row + 1; }
+    else { stan_Row = n_Row; }
+
+    double* sub_array = new double[col * stan_Row];
+    int* n_Element = new int[size];
+    int* n_displac = new int[size];
+
+    n_displac[0] = 0;
+    for (int i = 0; i < size; i++) {
+        if (i < rest_Row) { n_Element[i] = (n_Row + 1) * col; }
+        else { n_Element[i] = n_Row * col; }
+        if (i > 0) { n_displac[i] = n_displac[i - 1] + n_Element[i - 1]; }
+    }
+    MPI_Scatterv(array_temp, n_Element, n_displac, MPI_DOUBLE, sub_array, stan_Row * col, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    double* part_trans = new double[col];
+
+    for (int i = 0; i < n_displac[rank] / col; i++)
+    {
+        int rac = 0, sum = 0;
+        for (int j = 0; j < size; j++) {
+            sum += n_Element[j] / col;
+            if (i < sum) { rac = j; break; }  rac++;
+        }
+
+        MPI_Bcast(part_trans, col, MPI_DOUBLE, rac, MPI_COMM_WORLD);
+
+        for (int j = 0; j < n_Element[rank] / col; j++) {
+            double s = part_trans[i] / sub_array[j * col + i];
+            for (int z = i; z < col; z++) { sub_array[j * col + z] = s * sub_array[j * col + z] - part_trans[z]; }
+        }
+    }
+
+    for (int i = 0; i < n_Element[rank] / col; i++) {
+        for (int j = 0; j < col; j++) { part_trans[j] = sub_array[i * col + j]; }
+
+        MPI_Bcast(part_trans, col, MPI_DOUBLE, rank, MPI_COMM_WORLD);
+
+        for (int j = i + 1; j < n_Element[rank] / col; j++) {
+            double s = part_trans[n_displac[rank] / col + i] / sub_array[j * col + i + n_displac[rank] / col];
+            for (int z = i + n_displac[rank] / col; z < col; z++) {
+                sub_array[j * col + z] = s * sub_array[j * col + z] - part_trans[z];
             }
         }
     }
-    //displayVector(vec);
-    return vec;
-}
 
-std::vector<std::vector<double>> methodGaussParallel(std::vector<std::vector<double>> vec)
-{
-    vec = GenerateRandom();
-
-    //displayVector(vec);
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    int numberRow = vec.size() / size;
-    int rest_nRow = vec.size() % size;
-    int n_Element = numberRow * vec.size();
-    std::vector< std::vector<double>> sub_vec(n_Element);
-    for (int i = 0; i < n_Element; i++)
-    {
-        for (int j = 0; j < vec[i].size(); j++)
-        {
-            sub_vec[i][j] = vec[i][j];
-
+    int rac = 0;
+    for (int i = n_displac[rank] / col + n_Element[rank] / col; i < row; i++) {
+        int sum = 0;
+        for (int j = 0; j < size; j++) {
+            sum += n_Element[j] / col;
+            if (i < sum) { rac = j; break; }
+            rac++;
         }
-    }
-    std::vector<std::vector<double>> rest_vec(rest_nRow);
-    for (int i = numberRow; i < vec.size(); i++)
-    {
-        for (int j = 0; j < vec[i].size(); j++)
-        {
-            rest_vec[i][j] = vec[i][j];
 
-        }
+        MPI_Bcast(part_trans, col, MPI_DOUBLE, rac, MPI_COMM_WORLD);
     }
-    MPI_Scatter(&vec, numberRow * vec.size(), MPI_DOUBLE, &sub_vec, n_Element, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    MPI_Gatherv(sub_array, stan_Row * col, MPI_DOUBLE, array_temp, n_Element, n_displac, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-        if (rest_nRow != 0)
-            MPI_Bcast(&rest_nRow, rest_nRow * vec.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    }
-
-    std::vector<std::vector<double>> new_vec = methodGauss(vec);
-
-    for (int i = 0; i < n_Element; i++)
-    {
-        for (int j = 0; j < n_Element; j++)
-        {
-            sub_vec[i][j] = new_vec[i][j];
-        }
-
-    }
-    std::vector<std::vector<double>> new_subVec(rest_nRow);
-    for (int i = numberRow; i < vec.size(); i++)
-    {
-        for (int j = 0; j < vec[i].size(); j++)
-        {
-            rest_vec[i][j] = new_vec[i][j];
-
+        for (int i = row - 1; i >= 0; i--) {
+            double b = array_temp[i * col + col - 1];
+            for (int j = row - 1; j >= i + 1; j--) {
+                b -= array_temp[i * col + j] * solution[j];
+            }
+            solution[i] = b / array_temp[i * col + i];
         }
     }
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    MPI_Gather(&sub_vec, vec.size() * numberRow, MPI_DOUBLE, &vec, vec.size() * numberRow, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    if (rank == 0) {
-        if (rest_nRow != 0)
-            MPI_Gather(&rest_vec, vec.size() * rest_nRow, MPI_DOUBLE, &vec[numberRow], vec.size() * numberRow, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    std::cout << "\n --------------------------------------------------------------------------\n";
+    for (int i = 0; i < row * col; i++)
+    {
+        std::cout << std::setprecision(4) << std::setw(8) << std::fixed << array_temp[i];
+        if ((i + 1) % col == 0) { std::cout << std::endl; }
     }
-
-    // displayVector(vec);
-    return vec;
+    std::cout << "\n --------------------------------------------------------------------------\n";
+  
+    delete[] array_temp;
+    delete[] sub_array;
+    delete[] n_Element;
+    delete[] n_displac;
+    delete[] part_trans;
+    return array_temp;
 }
