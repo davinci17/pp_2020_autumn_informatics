d1cfd3672d1f79409c6a853a9e92834f2069059a
NADIR Mohammed <63372033+davinci17@users.noreply.github.com>
2020-11-23 12:29:51 -1200

    Update Methodgauss.h

--- modules/task_2/NADIR_METHOD_GAUSS/Methodgauss.h
+++ modules/task_2/NADIR_METHOD_GAUSS/Methodgauss.h
@@ -4,11 +4,9 @@
 
 #include<vector>
 
+std::vector<std::vector<double>> methodGauss(std::vector<std::vector<double>>array) {}
 
-void displayVector(std::vector<std::vector<double>> vec);
-std::vector<std::vector<double>> GenerateRandom();
-std::vector<std::vector<double>> methodGauss(std::vector<std::vector<double>> vec);
-std::vector<std::vector<double>> methodGaussParallel(std::vector<std::vector<double>> vec);
+double* methodGaussParallel(const double* array, double* solution) {}
 
 #endif // MODULES_TASK_2_NADIR_M_HORIZONTAL_METHOD_GAUSS_HORIZONTAL_METHOD_H_
 
