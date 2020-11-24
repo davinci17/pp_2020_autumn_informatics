5bb8ea17826c3dacc260c02b8a671963c5b5da7b
NADIR Mohammed <63372033+davinci17@users.noreply.github.com>
2020-11-24 00:52:03 -1200

    Update main.cpp

--- modules/task_2/NADIR_METHOD_GAUSS/main.cpp
+++ modules/task_2/NADIR_METHOD_GAUSS/main.cpp
@@ -3,15 +3,19 @@
 #include<mpi.h>
 #include "../../../modules/task_2/nadir_method_gauss/methodGauss.h"
 
+
 TEST(GAUSS_PARALLEL_MPI, 2x3)
 {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     int row = 2, col = 3;
-    double* array{ new double[row * col]{ 1,2,3,4,5,6} };
+    double* array= new double[row * col];
+    array[0] = 1; array[1] = 2;
+    array[2] = 3; array[3] = 4;
+    array[4] = 5; array[5] = 6;
     double* sub_solution1 = new double[row];
 
-    methodGaussParallel(array, sub_solution1, row, col);
+    methodGaussParallel(array, sub_solution1, row,col);
     if (rank == 0) {double* sub_solution2 = new double[row];
         methodGauss(array, sub_solution2, row);
         for (int i = 0; i < row; i++) {ASSERT_NEAR(sub_solution1[i], sub_solution2[i],0.00001);}
@@ -26,13 +30,17 @@ TEST(GAUSS_PARALLEL_MPI, 3x4)
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     int row = 3, col = 4;
-    double* array{ new double[row * col]{ 1,2,3,4,5,6,7,8,9,10,11,12 } };
+    double* array = new double[row * col];
+    array[0] = 1; array[1] = 2; array[2] = 3; 
+    array[3] = 4; array[4] = 5; array[5] = 6;
+    array[6] = 7; array[7] = 8; array[8] = 9;
+    array[9] = 10; array[10] = 11; array[11] = 12;
     double* sub_solution1 = new double[row];
 
-    methodGaussParallel(array, sub_solution1, row, col);
+    methodGaussParallel(array, sub_solution1,row ,col);
     if (rank == 0) {double* sub_solution2 = new double[row];
         methodGauss(array, sub_solution2, row);
-        for (int i = 0; i < row; i++) { ASSERT_NEAR(sub_solution1[i], sub_solution2[i],0.00001); }
+        for (int i = 0; i < row; i++) { ASSERT_EQ(sub_solution1[i], sub_solution2[i],0.00001); }
         delete[] sub_solution2;
     }
     delete[] array;
