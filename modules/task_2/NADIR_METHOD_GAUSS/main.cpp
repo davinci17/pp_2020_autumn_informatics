848b180269fa3f13f7c470116e6d7b4cae015d68
NADIR Mohammed <63372033+davinci17@users.noreply.github.com>
2020-11-25 02:01:40 -1200

    Update main.cpp

--- modules/task_2/NADIR_METHOD_GAUSS/main.cpp
+++ modules/task_2/NADIR_METHOD_GAUSS/main.cpp
@@ -5,54 +5,78 @@
 #include "./Methodgauss.h"
 
 
-
- TEST(GAUSS_PARALLEL_MPI, 2x3)
+TEST(GAUSS_PARALLEL_MPI, 2x3)
 {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     int row = 2, col = 3;
-    double* array= new double[row * col];
-    array[0] = 1; array[1] = 2;
-    array[2] = 3; array[3] = 4;
-    array[4] = 5; array[5] = 6;
-    double* sub_solution1 = new double[row];
 
-    methodGaussParallel(array, sub_solution1, row,col);
-    if (rank == 0) {double* sub_solution2 = new double[row];
-        methodGauss(array, sub_solution2, row);
-        for (int i = 0; i < row; i++) { 
-         
-           ASSERT_NEAR(sub_solution1[i], sub_solution2[i], 0.00001); 
+    double* array1{ new double[row * col]{ 1,2,3,4,5,6 } };
+    double* sub_solution1=methodGaussParallel(array1, row,col);
+
+
+
+    //**************************************
+    std::vector<std::vector<double>>array2{ {1,2,3} ,{4,5,6} };
+    std::vector<std::vector<double>>array_sol = methodGauss(array2, 2);
+    double* sub_solution2 = new double[row * col];
+    convertVector(array_sol, sub_solution2);
+
+    if (rank == 0) {
+        
+        for (int i = 0; i < row*col; i++)
+        { 
+            ASSERT_NEAR(sub_solution1[i], sub_solution2[i], 0.00001); 
         }
        
-        delete[] sub_solution2;
+       
     }
-    delete[] array;
+    delete[] array1;
     delete[] sub_solution1;
+    delete[] sub_solution2;
 }
 TEST(GAUSS_PARALLEL_MPI, 3x4)
 {
-    int rank, size;
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     int row = 3, col = 4;
-    double* array = new double[row * col];
-    array[0] = 1; array[1] = 2; array[2] = 3; 
-    array[3] = 4; array[4] = 5; array[5] = 6;
-    array[6] = 7; array[7] = 8; array[8] = 9;
-    array[9] = 10; array[10] = 11; array[11] = 12;
-    double* sub_solution1 = new double[row];
 
-    methodGaussParallel(array, sub_solution1,row ,col);
-    if (rank == 0) {double* sub_solution2 = new double[row];
-        methodGauss(array, sub_solution2, row);
-        for (int i = 0; i < row; i++) {
-          
+    double* array1{ new double[row * col]{ 1,2,3,4,5,6,7,8,9,10,11,12 } };
+    double* sub_solution1 = methodGaussParallel(array1, row, col);
 
-                ASSERT_NEAR(sub_solution1[i], sub_solution2[i], 0.00001);
-            }
-        delete[] sub_solution2;
+
+
+    //**************************************
+    std::vector<std::vector<double>>array2{ {1,2,3,4} ,{5,6,7,8},{9,10,11,12} };
+    std::vector<std::vector<double>>array_sol = methodGauss(array2, 3);
+    double* sub_solution2 = new double[row * col];
+    convertVector(array_sol, sub_solution2);
+
+    if (rank == 0) {
+
+        for (int i = 0; i < row * col; i++)
+        {
+            ASSERT_NEAR(sub_solution1[i], sub_solution2[i], 0.00001);
+        }
     }
-    delete[] array;
+    delete[] array1;
     delete[] sub_solution1;
+    delete[] sub_solution2;
+}
+
+
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
 }
